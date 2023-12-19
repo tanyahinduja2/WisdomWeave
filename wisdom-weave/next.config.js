0d4a031af97d68824a849da0e44878b2e4412330
tanyahinduja2 <tanyahinduja1@gmail.com>
2023-12-19 12:15:02 +0530

    final commit

--- wisdom-weave/next.config.js
+++ wisdom-weave/next.config.js
@@ -1,9 +1,12 @@
+const { default: build } = require('next/dist/build')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com'],
   },
+  distDir: build,
 }
 
 module.exports = nextConfig
